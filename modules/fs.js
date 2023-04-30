bf527640cf3d84e0d80b776a6009f5941f16487c
Guilherme Moreira <gf.moreira@hotmail.com.br>
2023-04-30 18:54:11 -0300

    try use path and fs

---
+++ modules/fs.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const path = require('path')
+
+fs.mkdir(path.join(__dirname, 'test'), (error) =>{
+    if (error) console.log('Error', error)
+
+    console.log('Pasta criada com sucesso!')
+   
+    fs.writeFile(path.join(__dirname, '/test', 'test.txt'), "Hello Node", (error) =>{
+        if (error) return console.log('Error', error)
+    
+        console.log('Arquivo criado com sucesso!')
+    
+        fs.appendFile(path.join(__dirname, '/test', 'test.txt'), " Hello Word", (error) =>{
+            if (error) return console.log('Error', error)
+        
+            console.log('Arquivo modificado com sucesso!')
+    
+            fs.readFile(path.join(__dirname, '/test', 'test.txt'), 'utf8', (error, data) =>{
+                if(error) return console.log("Erro: ", error)
+            
+                console.log(data)
+            })
+        })
+        
+    })
+})
+
+
